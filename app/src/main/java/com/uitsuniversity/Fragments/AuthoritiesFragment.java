0b768ee36d1d8bce4613c424fa9d0b20462db5b5
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2022-06-20 00:36:15 +0600

    Added ucam and solved err_clearText_not_permitted

--- app/src/main/java/com/uitsuniversity/Fragments/AuthoritiesFragment.java
+++ app/src/main/java/com/uitsuniversity/Fragments/AuthoritiesFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.uitsuniversity.Fragments.Authorities.ChairmanAuthorityFragment;
@@ -26,13 +27,13 @@ public class AuthoritiesFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_authorities, container, false);
 
-        TextView chairmanView = view.findViewById(R.id.chairman_text);
-        TextView boardView = view.findViewById(R.id.board_text);
-        TextView syndicateView = view.findViewById(R.id.syndicate_text);
-        TextView councilView = view.findViewById(R.id.council_text);
-        TextView chancellorView = view.findViewById(R.id.chancellor_text);
-        TextView proChancellorView = view.findViewById(R.id.pro_chancellor_text);
-        TextView treasurerView = view.findViewById(R.id.treasurer_text);
+        LinearLayout chairmanView = (LinearLayout) view.findViewById(R.id.chairman_text);
+        LinearLayout boardView = (LinearLayout) view.findViewById(R.id.board_text);
+        LinearLayout syndicateView = (LinearLayout) view.findViewById(R.id.syndicate_text);
+        LinearLayout councilView = (LinearLayout) view.findViewById(R.id.council_text);
+        LinearLayout chancellorView = (LinearLayout) view.findViewById(R.id.chancellor_text);
+        LinearLayout proChancellorView = (LinearLayout) view.findViewById(R.id.pro_chancellor_text);
+        LinearLayout treasurerView = (LinearLayout) view.findViewById(R.id.treasurer_text);
 
         //On click for chairman authority
         chairmanView.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/uitsuniversity/Fragments/UcamFragment.java
@@ -0,0 +1,44 @@
+package com.uitsuniversity.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.uitsuniversity.R;
+import com.uitsuniversity.WebViewController;
+
+public class UcamFragment extends Fragment {
+
+    public UcamFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_ucam, container, false);
+
+        WebView webView = view.findViewById(R.id.webview_ucam);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // for javascript support
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setSupportZoom(true);
+        webSettings.setDefaultTextEncodingName("utf-8");
+        webView.loadUrl("http://ucam.uits.edu.bd/Security/LogIn.aspx");
+        webView.setWebViewClient(new WebViewController());
+
+
+        return view;
+    }
+}
