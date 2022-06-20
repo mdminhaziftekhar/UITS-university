f71561d6a80862790736d6eeacff4448a4e44298
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2022-06-20 14:30:14 +0600

    Whole app complete without the bug fixes

--- app/src/main/java/com/uitsuniversity/Fragments/scholarships/RegularScholarshipsFragment.java
+++ app/src/main/java/com/uitsuniversity/Fragments/scholarships/RegularScholarshipsFragment.java
@@ -1,4 +1,4 @@
-package com.uitsuniversity.Fragments;
+package com.uitsuniversity.Fragments.scholarships;
 
 import android.os.Bundle;
 
@@ -13,18 +13,18 @@ import android.webkit.WebView;
 import com.uitsuniversity.R;
 import com.uitsuniversity.WebViewController;
 
-public class AdminstrationFragment extends Fragment {
-
-    public AdminstrationFragment() {
+public class RegularScholarshipsFragment extends Fragment {
+    public RegularScholarshipsFragment() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_adminstration, container, false);
+        View view = inflater.inflate(R.layout.fragment_regular_scholarships, container, false);
 
-        WebView webView = view.findViewById(R.id.webview_adminstration);
+        WebView webView = view.findViewById(R.id.webview_regular_waiver);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true); // for javascript support
         webSettings.setDomStorageEnabled(true);
@@ -34,7 +34,7 @@ public class AdminstrationFragment extends Fragment {
         webSettings.setDisplayZoomControls(false);
         webSettings.setSupportZoom(true);
         webSettings.setDefaultTextEncodingName("utf-8");
-        webView.loadUrl("https://uits.edu.bd/office-of-the-board-of-trustees/");
+        webView.loadUrl("https://uits.edu.bd/scholarships-and-financial-aid/");
         webView.setWebViewClient(new WebViewController());
 
         return view;
