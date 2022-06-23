a371b7b72090826e10be6027b163cbf27c579782
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2022-06-23 21:51:58 +0600

    added messenger support

--- app/src/main/java/com/uitsuniversity/WebViewController.java
+++ app/src/main/java/com/uitsuniversity/WebViewController.java
@@ -10,7 +10,15 @@ public class WebViewController extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        view.loadUrl(url);
+        if(url.startsWith("intent:")) //this is for the messenger functionality in app
+        {
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.me/theuits/"));
+            view.getContext().startActivity(i);
+        }
+        else{
+            view.loadUrl(url);
+        }
         return true;
     }
+
 }
